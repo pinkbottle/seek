439d45930a07e383e122d3a0a49d949f0e4531cb
charconstpointer <attwistingnether@gmail.com>
2022-01-27 21:05:30 +0100

    chore: remove run-compose.sh

--- build/run-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🌘 building crawler"
-SHA=$(bash ./build-crawler.sh)
-echo "🌗 setting CRAWLER_IMAGE"
-export CRAWLER_IMAGE=$SHA
-echo "🌕 running docker-compose.yaml"
-cd ../ && docker-compose -f docker-compose.yaml up -d
